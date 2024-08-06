56ec5671e124edb3b5317b82d743df7b821f336f
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-08-05 23:16:49 -0300

    add: task entity

---
+++ src/main/java/com/jcompany/task_management_system/entities/Task.java
@@ -0,0 +1,17 @@
+package com.jcompany.task_management_system.entities;
+
+import java.util.Date;
+
+public class Task {
+
+	private Long idTask;
+	
+	private String description;
+	
+	private boolean done;
+	
+	private Date dateCreated;
+	
+	private Date dateDone;
+	
+}
