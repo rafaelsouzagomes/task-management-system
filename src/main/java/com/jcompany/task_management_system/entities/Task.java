f79afb16b8465bac0a517be193211a558baea3dd
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-08-06 23:04:09 -0300

    add: task properties in entity

--- src/main/java/com/jcompany/task_management_system/entities/Task.java
+++ src/main/java/com/jcompany/task_management_system/entities/Task.java
@@ -2,8 +2,17 @@ package com.jcompany.task_management_system.entities;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Task {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idTask;
 	
 	private String description;
@@ -13,5 +22,64 @@ public class Task {
 	private Date dateCreated;
 	
 	private Date dateDone;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+
+	public void markDone() {
+		done = true;
+	}
+	
+	public void uncheck() {
+		done = false;
+	}
+
+	public Long getIdTask() {
+		return idTask;
+	}
+
+	public void setIdTask(Long idTask) {
+		this.idTask = idTask;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getDateDone() {
+		return dateDone;
+	}
+
+	public void setDateDone(Date dateDone) {
+		this.dateDone = dateDone;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
+
 	
 }
