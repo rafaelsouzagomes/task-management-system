0b786e87a0a38cce125568cf4227ba14f31f4dab
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-08-19 23:28:34 -0300

    variable token

--- src/main/java/com/jcompany/task_management_system/security/JwtAuthenticationFilter.java
+++ src/main/java/com/jcompany/task_management_system/security/JwtAuthenticationFilter.java
@@ -2,19 +2,22 @@ package com.jcompany.task_management_system.security;
 
 import java.io.IOException;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Value("${jwt.secret}")
+    private String secretKey;
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
@@ -28,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         String token = header.substring(7);
         Claims claims = Jwts.parser()
-                .setSigningKey("your_secret_key")  // Alterar para sua chave secreta
+                .setSigningKey(secretKey)  
                 .parseClaimsJws(token)
                 .getBody();
 
